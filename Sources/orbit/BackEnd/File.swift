310ab7270ce41084f9c53db59853eecf281a3aa1
CAOCAP <ficruty@gmail.com>
2023-08-30 05:52:15 +0300

    File structures

---
+++ Sources/orbit/BackEnd/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Azzam AL-Rashed on 30/08/2023.
+//
+
+import Foundation
---
+++ Sources/orbit/FrontEnd/Tokenizer.swift
@@ -0,0 +1,8 @@
+//
+//  Tokenizer.swift
+//  
+//
+//  Created by Azzam AL-Rashed on 30/08/2023.
+//
+
+import Foundation
